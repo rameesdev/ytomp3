c39ae7b155e00341e93a62b6293321ed34221e09
muhammadramees697 <muhammadramees697@gmail.com>
2024-05-01 02:23:06 +0000

    seo optimisation

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 git add .
-git commit -m "devlopment"
+git commit -m "seo optimisation"
 git push origin glitch
-curl https://api.render.com/deploy/srv-cmnufoen7f5s73d17rq0?key=rxXRkGKJvqs
+curl https://api.render.com/deploy/srv-cmsedif109ks73dut8cg?key=GMbIwu1aU4w
